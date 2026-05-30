fbf9bec99f2db0f25f6d45bc6143c86f2813330a
Prince Adams <adamsprince720@gmail.com>
2026-05-30 02:32:33 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Prince Adams
+# STUDENT ID: 2425402522
+# Email: 2425402522@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor,Prince Adams, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials in stylized large letters using the letters P, A, and K.
+    # Format: blank line, "..." before small initials, blank line,
+    # "..." before and after large block letters, blank line.
 
-
+    print()
+    print("...PAK")
+    print()
+    print("...PPPPPPPPPP.............AAAAAA...........KK.......KK......")
+    print("...PP.........P..........AAA..AAA..........KK......KK.......")
+    print("...PP.........P.........AAA....AAA.........KK.....KK........")
+    print("...PPPPPPPPPP..........AAA......AAA........KK....KK.........")
+    print("...PP..................AAAAAAAAAAAA........KKKKKK...........")
+    print("...PP..................AAA......AAA........KK....KK.........")
+    print("...PP..................AAA......AAA........KK.....KK........")
+    print("...PP..................AAA......AAA........KK......KK.......")
+    print("...PP.............**...AAA......AAA...**...KK.......KK....**")
+    print("...PP.............**...AAA......AAA...**...KK........KK...**")
+    print()
 
 main()
